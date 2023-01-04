1423455c31b19c9c2589235937af646d51773aca
Victor <rainbowkiwifox@gmail.com>
2023-01-04 03:04:36 +0300

    Fixed Flipper loading in DFU mode

--- lib/drivers/WS2812B.h
+++ lib/drivers/WS2812B.h
@@ -1,9 +1,20 @@
 /*
- * WS2812B.h
- *
- * Created: 03.05.2018 20:28:20
- *  Author: Quenon
- */
+    WS2812B FlipperZero driver
+    Copyright (C) 2022  Victor Nikitchuk (https://github.com/quen0n)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #ifndef WS2812B_H_
 #define WS2812B_H_
